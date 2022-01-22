6f923e612d538d6128a25a119c22a9c80dcae394
qq449245884 <449245884@qq.com>
2022-01-22 16:22:50 +0800

    feat: 01 小程序分包配置

---
+++ src/pagesB/detail/detail.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="content">
+   分包B
+  </view>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  data() {
+    return {
+      title: 'Hello',
+    };
+  },
+  onLoad() {},
+  methods: {},
+});
+</script>
+
+<style></style>
