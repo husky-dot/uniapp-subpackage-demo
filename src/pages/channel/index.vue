fc44057151438f9c5deea68fbe1d7a1134cb8fcc
qq449245884 <449245884@qq.com>
2022-01-22 21:35:10 +0800

    feat: 完成事例

---
+++ src/pages/channel/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="content">
+    MemberChannel
+	</view>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+	export default Vue.extend({
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	});
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/member/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="content">
+    Member
+	</view>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+	export default Vue.extend({
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	});
+</script>
+
+<style>
+
+</style>
